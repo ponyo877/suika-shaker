9552a6535bfc0d47549fc471f8892fde8ab20e49
ponyo877 <keisuke877jp@yahoo.co.jp>
2025-11-02 11:51:57 +0900

    add game sounds

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ import (
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 	"github.com/hajimehoshi/ebiten/v2/inpututil"
 	"github.com/hajimehoshi/ebiten/v2/vector"
-	"github.com/ponyo877/suika-shaker/assets"
+	assets "github.com/ponyo877/suika-shaker/assets/image"
 
 	"github.com/jakecoffman/cp/v2"
 )
